ba561e2eb8b2420e39eff53310a83b45616f4fad
samuel <samuelhww@gmail.com>
2019-11-13 15:04:47 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,23 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [{
+  rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png"
+},
+{
+  rank: "queen",
+  suit: "diamonds",
+  cardImage: "images/queen-of-diamonds.png"
+},
+{
+  rank: "king",
+  suit: "hearts",
+  cardImage: "images/king-of-hearts.png"
+},
+{
+  rank: "king",
+  suit: "diamonds",
+  cardImage: "images/king-of-diamonds.png"
+}];
 
 var cardsInPlay = [];
 
@@ -11,12 +30,14 @@ function checkForMatch() {
 };
 
 function flipCard(cardId) {
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
   if (cardsInPlay.length === 2) {
       checkForMatch();
-  }
+  };
 };
 
 flipCard(0);
-flipCard(2);
+flipCard(1);
